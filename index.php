12f747b8bfdbb80a892be771118ba3c54f85ceb5
RedMeca <tytydodo67140@gmail.com>
2017-05-27 15:04:21 +0200

    Begining
    Added index.php and style.css and the Bootstrap distribution to the folder

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>UniversWar</title>
+        <link href="bootstrap-4.0.0-alpha.6-dist/css/bootstrap.min.css"rel="stylesheet"/>
+        <link href="style.css" rel="stylesheet"/>
+    </head>
+    <body>
+    
+    </body>
+</html>
